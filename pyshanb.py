7faec011d88acf16a475b50a6396c5de112db655
Mozillazg <opensource.mozillazg@gmail.com>
2012-11-13 20:52:28 +0800

    调整注释

--- pyshanb.py
+++ pyshanb.py
@@ -16,11 +16,15 @@ def login(url_login, headers, username, password):
     # 首先访问一次网站，获取 cookies
     r_first_vist = requests.get(url_login, headers=headers,
                                 prefetch=False,)
+    # 判断 HTTP 状态码是否是 200
     if r_first_vist.status_code != requests.codes.ok:
         return None
+    # 获取 cookies 信息
     cookies_first_vist = r_first_vist.cookies.get_dict()
+
+    # 准备用于登录的信息
     url_post = url_login
-    # 获取用于用户标识的 cookies
+    # 获取用于用户标识的 cookies（身份令牌）
     token = cookies_first_vist.get('csrftoken')
     # 设置 headers
     headers_post = copy.deepcopy(headers)
@@ -39,6 +43,7 @@ def login(url_login, headers, username, password):
         'u': 1,
         'next': '',
     }
+
     # 提交登录表单并提交第一次访问网站时生成的 cookies
     r_login = requests.post(url_post, headers=headers_post,
                             cookies=cookies_post,
@@ -87,6 +92,7 @@ def get_example(api, headers, cookies, learning_id):
     if r_example.status_code != requests.codes.ok:
         return None
     example_json = r_example.json
+    # 判断是否包含例句信息
     if example_json.get('examples_status') != 1:
         return None
     new_cookies = r_example.cookies.get_dict()
@@ -125,8 +131,8 @@ def check_error(func):
 
 @check_error
 def main():
-    """docstring for main"""
     import sys
+
     if sys.version_info[0] == 3:
         print u"Sorry, this program doesn't support Python 3 yet"
         exit(0)
@@ -137,29 +143,33 @@ def main():
     import time
     import conf
 
-
     site = conf.site
     username = conf.username
     password = conf.password
-    auto_play = conf.auto_play
+    auto_play = conf.auto_play  # 自动播放单词读音
+
     if auto_play and os.name == 'nt':
         import mp3play
-    auto_add = conf.auto_add
-    ask_add = conf.ask_add
-    enable_en_definition = conf.enable_en_definition
-    enable_example = conf.enable_example
+
+    auto_add = conf.auto_add  # 自动保存单词到扇贝网
+    ask_add = conf.ask_add  # 询问是否保存单词
+    enable_en_definition = conf.enable_en_definition  # 单词英文释义
+    enable_example = conf.enable_example  # 用户自己添加的单词例句
     url_login = site + conf.url_login
-    api_get_word = site + conf.api_get_word
-    api_get_example = site + conf.api_get_example
-    api_add_word = site + conf.api_add_word
+    api_get_word = site + conf.api_get_word  # 获取单词信息的 api
+    api_get_example = site + conf.api_get_example  # 获取例句的 api
+    api_add_word = site + conf.api_add_word  # 保存单词的 api
+
     headers = {
         'Host': urlparse.urlsplit(site).netloc,
         'User-Agent': ('Mozilla/5.0 (Windows NT 6.2; rv:15.0) Gecko'
                        + '/20100101 Firefox/15.0.1'),
     }
+
     # 登录
     print 'Login...'
     cookies = login(url_login, headers, username, password)
+
     if cookies:
         while True:
             word = quote(raw_input('Please input a english word: ').strip())
@@ -167,6 +177,7 @@ def main():
                 continue
             # 获取单词信息
             result_get = get_word(api_get_word, headers, cookies, word)
+
             if result_get:
                 # 输出单词信息
                 # 学习记录
@@ -187,6 +198,7 @@ def main():
                                                           ).iteritems()]
                 # 中文解释
                 word_definition = voc.get(u'definition')
+
                 print u'-' * 55
                 # print u'%s [%s]' % (word_content, word_pron)
                 print u'%s' % (word_content)
@@ -194,7 +206,7 @@ def main():
                 if enable_en_definition and word_en_definition:
                     for en in word_en_definition:
                         print u'%s' % (en)
-                word_examples = list()
+
                 if auto_play and os.name == 'nt':
                     # 临时保存音频文件
                     file_name = (str(time.time()) +
@@ -211,6 +223,7 @@ def main():
                    # 移除临时文件
                     os.remove(temp_file)
                     # print os.path.exists(temp_file)
+
                 if auto_add or ask_add:
                     # 如果未收藏该单词
                     if word_leaning_id == 0:
@@ -229,6 +242,7 @@ def main():
                                                        word)[u'id']
 
                 # 例句
+                word_examples = list()
                 if enable_example and word_leaning_id != 0:
                     word_example = get_example(api_get_example, headers,
                                                cookies, word_leaning_id)
@@ -241,6 +255,7 @@ def main():
                 if enable_example and word_examples:
                     for ex in word_examples:
                         print u'%s' % (ex)
+
                 print u'-' * 55
     else:
         print u'Login failed!'

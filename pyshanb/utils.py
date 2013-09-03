bb78b60e4bd9eabe526e68cd35b1d33869c3fe34
Mozillazg <opensource.mozillazg@gmail.com>
2013-09-03 09:24:19 +0800

    更改 from pyshanb.xxx import yyy 为 from .xxx import yyy

--- pyshanb/utils.py
+++ pyshanb/utils.py
@@ -7,8 +7,8 @@
 import os
 from getpass import getpass
 
-from pyshanb.cmdoption import CmdOption
-from pyshanb.conf import Settings
+from .cmdoption import CmdOption
+from .conf import Settings
 
 
 # Modified from https://github.com/webpy/webpy/blob/master/web/utils.py
